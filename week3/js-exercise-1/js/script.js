7331fac5d6559fdcf721003fd5b6e89a281be9a0
PhyllisFei <36285821+PhyllisFei@users.noreply.github.com>
2020-04-09 17:24:36 +0800

    Add files via upload

---
+++ week3/js-exercise-1/js/script.js
@@ -0,0 +1,11 @@
+function openNewWindow() {
+	window.open("images/kitten2.jpg","_blank","width=20, height=30, left=100, top=100");
+}
+
+function changeText() {
+	document.getElementById("header").innerHTML = prompt("Oh, no! So Much Work!");
+}
+
+function changeImages() {
+	document.getElementById("images").style.width = "0px";
+}
